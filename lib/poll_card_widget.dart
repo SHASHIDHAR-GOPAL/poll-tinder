9370b8bcaa8d1e9c37c50640a82c0ad6e56a99f0
Shashidhara Gopal <shashidhara@votercircle.com>
2019-06-01 11:00:42 +0530

    Add image from assets

---
+++ lib/poll_card_widget.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class PollCardWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Image.asset('assets/placeholder_image.png'),
+    );
+  }
+}
